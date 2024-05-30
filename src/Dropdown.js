67f18f71bd36da10b352b7ba8215c9df41f487bf
Akriti Keswani <akritikeswani@Akritis-Air.attlocal.net>
2024-05-29 22:41:55 -0700

    fixing some stuff

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -3,6 +3,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./Dropdown.css";
+import { FaCaretDown } from "react-icons/fa"; // Import the arrow icon
 
 const Dropdown = ({
   options,
@@ -14,19 +15,24 @@ const Dropdown = ({
   const [isOpen, setIsOpen] = useState(false);
   const containerRef = useRef(null);
 
+  // Function to handle option selection
   const handleOptionClick = (option) => {
     if (multiSelect) {
       if (selectedOptions.includes(option)) {
+        // If multiSelect is enabled and option is already selected, deselect it
         onChange(selectedOptions.filter((item) => item !== option));
       } else {
+        // If multiSelect is enabled and option is not selected, select it
         onChange([...selectedOptions, option]);
       }
     } else {
+      // If multiSelect is not enabled, select the option and close the dropdown
       onChange([option]);
       setIsOpen(false);
     }
   };
 
+  // Function to handle click outside of the dropdown to close it
   const handleDocumentClick = (event) => {
     if (containerRef.current && !containerRef.current.contains(event.target)) {
       setIsOpen(false);
@@ -43,11 +49,14 @@ const Dropdown = ({
   return (
     <div className="dropdown" ref={containerRef}>
       <div className="dropdown-header" onClick={() => setIsOpen(!isOpen)}>
-        {selectedOptions.length > 0
-          ? multiSelect
-            ? selectedOptions.join(", ")
-            : selectedOptions[0]
-          : placeholder}
+        <span>
+          {selectedOptions.length > 0
+            ? multiSelect
+              ? selectedOptions.join(", ")
+              : selectedOptions[0]
+            : placeholder}
+        </span>
+        <FaCaretDown className="dropdown-arrow" />
       </div>
       {isOpen && (
         <ul className="dropdown-list">
@@ -74,11 +83,11 @@ const Dropdown = ({
 };
 
 Dropdown.propTypes = {
-  options: PropTypes.array.isRequired,
-  selectedOptions: PropTypes.array,
-  onChange: PropTypes.func.isRequired,
-  multiSelect: PropTypes.bool,
-  placeholder: PropTypes.string,
+  options: PropTypes.array.isRequired, // Array of options to display in the dropdown
+  selectedOptions: PropTypes.array, // Array of currently selected options
+  onChange: PropTypes.func.isRequired, // Function to call when selected options change
+  multiSelect: PropTypes.bool, // Boolean to enable/disable multi-select functionality
+  placeholder: PropTypes.string, // Placeholder text when no option is selected
 };
 
 Dropdown.defaultProps = {
